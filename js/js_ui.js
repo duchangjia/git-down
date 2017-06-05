0c3caa2f12d76ad8c9b4b264ccef9aa1c4acf457
aduker <954920354@qq.com>
2017-06-05 11:30:10 +0800

    手机金融网站

---
+++ js/js_ui.js
@@ -0,0 +1,17 @@
+﻿var DocumentInit = {
+	
+	//基础初始化
+	BasicInit : function() {
+		DocumentInit.mainMenuSwitch();
+	},
+	
+	mainMenuSwitch:function() {
+		$("#header_menu .main_menu_switch").click( function(){$("#main_menu").slideToggle("fast"); return false; });
+	}
+
+	
+};
+
+$(document).ready(function() {
+	DocumentInit.BasicInit();
+});
